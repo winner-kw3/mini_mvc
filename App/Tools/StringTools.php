4b3ade0beb748a72a63fe3530a273b4a52d584b0
Anthony L <laplane_anthony@yahoo.fr>
2024-10-02 14:42:10 +0200

    Update StringTools.php

--- App/Tools/StringTools.php
+++ App/Tools/StringTools.php
@@ -38,19 +38,19 @@ class StringTools
     public static function slugify($text, string $divider = '-')
     {
         // replace non letter or digits by divider
-        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+        $text = preg_replace('/[^\pL\d.]+/u', $divider, $text);
 
         // transliterate
         $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
 
         // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = preg_replace('/[^-\w.]+/', '', $text);
 
         // trim
         $text = trim($text, $divider);
 
         // remove duplicate divider
-        $text = preg_replace('~-+~', $divider, $text);
+        $text = preg_replace('/-+/', $divider, $text);
 
         // lowercase
         $text = strtolower($text);
