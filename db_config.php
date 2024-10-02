73e620978103ce6f15d871d42c852125734bbb1d
Anthony Laplane <anthony.laplane@gmail.com>
2024-10-02 17:32:13 +0200

    adding .env

--- db_config.php
+++ db_config.php
@@ -1,8 +1,3 @@
 <?php
-    return [
-        'db_name' => 'mini_website',
-        'db_user' => 'root',
-        'db_password' => '',
-        'db_port' => '3306',
-        'db_host' => 'localhost'
-    ];
+$env = parse_ini_file('.env');
+return $env;
