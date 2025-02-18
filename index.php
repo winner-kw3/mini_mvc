63d188dac54939b7ea93b9c629978f379436c741
Anthony Laplane <anthony.laplane@gmail.com>
2025-02-18 10:56:35 +0100

    remove session settings

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 require_once __DIR__.'/config.php';
 
-// Sécurise le cookie de session avec httponly
-session_set_cookie_params([
-    'lifetime' => 3600,
-    'path' => '/',
-    'domain' => $_SERVER['SERVER_NAME'],
-    'httponly' => true
-]);
 session_start();
 define('_ROOTPATH_', __DIR__);
 define('_TEMPLATEPATH_', __DIR__.'/templates');
